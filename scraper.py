ff763da9f91b7549b9a5d52e468d5272ebc35fb4
ThatsLeo <leonardorocco.monaco@studio.unibo.it>
2026-09-18 01:10:19 +0200

    funzione yield di fetch pagine

--- scraper.py
+++ scraper.py
@@ -1,10 +1,7 @@
-from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import HTTPError
 import time
 import json
-import asyncio
-import yt_dlp
 
 
 ANI_URL = "https://graphql.anilist.co"
@@ -16,8 +13,10 @@ DOWNLOAD_URL = "https://naedist.animemusicquiz.com"
 #ritorna un {False: errore} in caso di errore
 #ritorna un json in caso di successo
 #Ogni numero "page" carica una lista di 20 item.
-def ani_query(page, type="ANIME", format="TV", ANI_URL = ANI_URL):
+#Yield function da usare in un loop, es: for data in ani_query(...):
+def ani_query(page_start=1, page_end=None, type="ANIME", format="TV", ANI_URL = ANI_URL):
 
+    
     query = """
         query($page:Int = 1, $type:MediaType, $format:[MediaFormat], 
         $sort:[MediaSort]=[POPULARITY_DESC,SCORE_DESC] ) { 
@@ -39,26 +38,42 @@ def ani_query(page, type="ANIME", format="TV", ANI_URL = ANI_URL):
             }
         }
         """
+    page = page_start
 
-    variables = {
-        "page": page,
-        "type": type,
-        "format": [format],
-        "sort": "SCORE_DESC"
-        }
-    
-    try:
-        response = requests.post(ANI_URL, json={"query":query, "variables":variables})
-        response.raise_for_status()
+    while True:
+        variables = {
+            "page": page,
+            "type": type,
+            "format": [format],
+            "sort": "SCORE_DESC"
+            }
+        
+        try:
+            response = requests.post(ANI_URL, json={"query":query, "variables":variables})
+            response.raise_for_status()
 
-    except HTTPError as http_err:
-        return {False: http_err}
+        except HTTPError as http_err:
+            yield {False: http_err}
+            return
 
-    except Exception as err:
-        return {False: err}
+        except Exception as err:
+            yield {False: err}
+            return
 
-    else:
-        return response.json()
+        else:
+            res = response.json()
+            yield res
+
+        info = res["data"]["Page"]["pageInfo"]
+
+        if not info["hasNextPage"]:
+            return
+        
+        if page_end is not None and page >= page_end:
+            return
+
+        page += 1
+        time.sleep(1)
 
 
 #la funzione deve essere chiamata con una lista di id, se è solo uno allora [id]
@@ -85,7 +100,8 @@ def cerca_anisongdb(mal_ids, filters = None, url = THEME_URL):
     risultati = response.json()
     return risultati
 
-
+#carica il db e lo restituisce come file.
+#non utilizzata al momento.
 def load_db(path):
     try:
         with open(path, "r", encoding="utf-8") as f:
@@ -93,76 +109,111 @@ def load_db(path):
     except FileNotFoundError:
         return None
 
-
+#aggiunge una entry nel db.
 def add_entry(entry, path):
     with open(path, "a", encoding="utf-8") as f:
-        riga = json.dumps(entry, indent=4, ensure_ascii=False)
+        riga = json.dumps(entry, ensure_ascii=False)
         f.write(riga + "\n")
 
+#god function che prende una pagina start e una target, scarica tutte le pagine di mezzo
+#e le scrive nel db.
+#Page_end è COMPRESA nelle pagine scaricate.
+#Se page_end è None la funzione continua fin quando trova pagine da anilist.
+#sleep attende secondi per evitare timeout dai server.
+def popolate(page_start, page_end, path, sleep=None):
 
-def popolate(sleep=None):
-    data = ani_query(1)
-    data = data["data"]["Page"]["media"]
+    for data in ani_query(page_start, page_end):
+        if False in data:
+            print("Errore:", data[False])
+            break
 
-    for anime in data:
-        
-        nameJP = anime["title"]["userPreferred"]
-        nameEN = anime["title"]["english"]
-        idMal = anime["idMal"]
-        idAnilist = anime["id"]
-        cover_img = anime["coverImage"]
-        print(f"{nameEN}:{idMal}")
+        media = data["data"]["Page"]["media"]
 
-        if sleep:
-            time.sleep(sleep)
+        for anime in media:
+            
+            nameJP = anime["title"]["userPreferred"]
+            nameEN = anime["title"]["english"]
+            idMal = anime["idMal"]
+            idAnilist = anime["id"]
+            cover_img = anime["coverImage"]
+            print(f"{nameEN}:{idMal}")
 
-        temi = cerca_anisongdb([idMal])
-        if temi == None:
-            print(temi)
-        else:
-            openings = {}
-            endings = {}
-            for tema in temi:
-                diff = tema["songDifficulty"]
-                type = tema["songType"]
-                song = tema["songName"]
-                video_id = tema.get("HQ") or tema.get("MQ")
-                audio_id = tema.get("audio")
+            if sleep:
+                time.sleep(sleep)
+
+            temi = cerca_anisongdb([idMal])
+            if temi == None:
+                print(temi)
+            else:
+                openings = {}
+                endings = {}
+                for tema in temi:
+                    diff = tema["songDifficulty"]
+                    type = tema["songType"]
+                    song = tema["songName"]
+                    video_id = tema.get("HQ") or tema.get("MQ")
+                    audio_id = tema.get("audio")
 
 
-                song_info = {
-                "song": song,
-                "difficulty": diff,
-                "video": video_id,
-                "video_path": None,
-                "audio": audio_id,
-                "audio_path": None
+                    song_info = {
+                    "song": song,
+                    "difficulty": diff,
+                    "video": video_id,
+                    "video_path": None,
+                    "audio": audio_id,
+                    "audio_path": None
+                    }
+
+                    if "Opening" in type:
+                        openings[type] = song_info
+                    elif "Ending" in type:
+                        endings[type] = song_info
+
+                new = {
+                    "entry": {
+                        "anilist_id":idAnilist,
+                        "mal_id":idMal,
+                        "nameEN":nameEN,
+                        "nameJP":nameJP,
+                        "coverImg": cover_img,
+                        "openings": openings,
+                        "Endings": endings,
+
+                    }
                 }
-
-                if "Opening" in type:
-                    openings[type] = song_info
-                elif "Ending" in type:
-                    endings[type] = song_info
-
-            new = {
-                "entry": {
-                    "anilist_id":idAnilist,
-                    "mal_id":idMal,
-                    "nameEN":nameEN,
-                    "nameJP":nameJP,
-                    "coverImg": cover_img,
-                    "openings": openings,
-                    "Endings": endings,
-
-                }
-            }
-            add_entry(new, "db.jsonl")
+                add_entry(new, path)
 
 
 
 #scarica dal dominio secondo il codice riportato nel database.
-def download(code):
-    pass
+#LA DEVO FINIRE
+def check_media_path(code):
+    with open("db.jsonl", "r", encoding="utf-8") as f:
+
+        for index in range():
+            riga = f[index].strip()
+            if riga:
+                entry = json.loads(riga)
+                print(entry)
 
 
-popolate()
+
+
+def scarica_file(url, percorso_destinazione):
+    if not url:
+        return False
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        with open(percorso_destinazione, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        return True
+    except Exception as err:
+        print(f"Errore scaricando {url}: {err}")
+        return False
+                
+
+
+#popolate(1, 2, "db.jsonl", 2)
+check_media_path(1)
